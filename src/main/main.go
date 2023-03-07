bebc5b3380a8c1b64ea3cb0da0c702d8c0d81258
VicentevonEverec <vjmr1@alu.ua.es>
2023-03-07 14:13:13 +0100

    Main corregido

--- src/main/main.go
+++ src/main/main.go
@@ -40,14 +40,14 @@ import (
 func main() {
 
 	fmt.Println("sdshttp :: un ejemplo de login mediante TLS/HTTP en Go.")
-	s := "Introduce srv para funcionalidad de servidor y cli para funcionalidad de cliente"
+	s := "Introduce server para funcionalidad de servidor y cliente para funcionalidad de cliente"
 
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
-		case "srv":
+		case "server":
 			fmt.Println("Entrando en modo servidor...")
 			server.Run()
-		case "cli":
+		case "cliente":
 			fmt.Println("Entrando en modo cliente...")
 			cliente.Run()
 		default:
